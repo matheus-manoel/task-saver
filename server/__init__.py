5276fd87db3a6c2699782aa058233970171a87aa
Matheus Manoel <matheuscmanoel@gmail.com>
2017-07-12 16:51:04 -0300

    Add initial configurations.

---
+++ server/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, Blueprint, request
+from flask_mongoengine import MongoEngine
+from server.routes import create_routes
+from server import utils
+from server import models
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(create_routes(Blueprint, request, utils, models))
+
+    app.config['MONGODB_SETTINGS'] = {
+            'db': 'task-saver',
+            'host': 'mongodb://localhost:27017',
+            'connect': False
+    }
+
+    db = MongoEngine()
+    db.initi_app()
+
+    return app
